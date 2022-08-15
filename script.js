fb2f11f8e8f10344e67293dcf93980b9be7b6650
Prashant Bhardwaj <geekyprashant840@gmail.com>
2022-08-15 22:38:27 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Hamburger Menu
+
+var menu = document.getElementById("bar");
+var item = document.getElementById("item");
+
+item.style.right = "-300px";
+menu.onclick = function () {
+  if (item.style.right == "-300px") {
+    item.style.right = "0";
+  } else {
+    item.style.right = "-300px";
+  }
+};
